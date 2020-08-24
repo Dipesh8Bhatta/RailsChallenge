000a24adb58d99c48297be0c912ed60ea5f72f33
dipesh.8bhatta <dipesh.8bhatta@gmail.com>
2020-08-25 08:35:58 +1000

    Add Simple Kickchart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  require 'json'
+
   def index
     @consignments = Consignment.all
   end
+
+  def chart
+    @chart_data = Consignment.select(:consignment_type).group(:consignment_type).count
+    return render json: @chart_data.to_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'chart', to: 'home#chart', as: 'chart'
 
   post 'search', to: 'consignments#search', as: 'search'
 
